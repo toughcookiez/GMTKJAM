ae8d90654e95d46dcacfa7b554b442e17c3230c5
tommy <tommypeled10@gmail.com>
2025-08-01 22:20:32 +0200

    fixed an issue with the break particle effect

--- Assets/Scripts/DestroyRadius.cs
+++ Assets/Scripts/DestroyRadius.cs
@@ -22,6 +22,8 @@ public class DestroyRadius : MonoBehaviour
         if (other.gameObject.CompareTag("SpawnObject") || other.gameObject.CompareTag("Block") || other.gameObject.CompareTag("Harmful"))
         {
             Destroy(other.gameObject);
+            GameObject breakEffect = Instantiate(player.BlockBreakEffect, other.gameObject.transform.position, Quaternion.identity);
+            breakEffect.GetComponent<ParticleSystem>().startColor = other.gameObject.GetComponent<BlockColor>()._breakColor;
 
             //Get points for destroyed objects
             player.points += player.pointsPerDestroyedObject;
